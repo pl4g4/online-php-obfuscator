8aaf1a410819a0df92be8529dd09ea1e443d2664
pl4g4 <cyberboy18@gmail.com>
2012-11-20 11:53:01 -0600

    changed donation form

--- obfuscator/index.php
+++ obfuscator/index.php
@@ -289,12 +289,8 @@ function curPageName() {
           <?php
           
           echo '
-          <form style="margin: 11px 90px 20px;" id="donationForm" name="donationForm" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="5N6K3MF8HSTKL">
-<input type="image" src="https://www.paypalobjects.com/WEBSCR-640-20110429-1/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/WEBSCR-640-20110429-1/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
+          
+          Your donations form goes here 
 
 				';
 	?>
